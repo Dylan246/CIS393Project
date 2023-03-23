6705d9abc705d77b589a47566b2460a75da96e70
Dylan246 <dylan.gazda@gmail.com>
2023-03-22 23:42:48 -0500

    Checkout submit order button fixed

--- cart.js
+++ cart.js
@@ -4,6 +4,9 @@ function loadedHandler() {
     let deliveryIndicator = document.getElementById("option")
     deliveryIndicator.addEventListener("input", optionChange)
 
+    let submit = document.getElementById("form")
+    submit.addEventListener("submit", onClick)
+
     if (document.getElementById("checkoutCart")) { //Update cart on checkout page
         let checkoutCart = document.getElementById("checkoutCart")
         var array = JSON.parse(sessionStorage.getItem('theCart'))
@@ -24,5 +27,17 @@ function optionChange(event) { //If delivery is selected, show needed delivery d
             document.getElementById("delivery").hidden = false
         } else
             document.getElementById("delivery").hidden = true
+    }
+}
+
+function onClick(event) { //Alert that order was sent, clear cart 
+    if (event) {
+        let val = event.target.value
+        let items2 = []
+        sessionStorage.setItem("theCart", JSON.stringify(items2))
+        let cartList = document.getElementById('checkoutCart')
+        cartList.innerHTML = ""
+
+        alert("Your order was submitted. You'll get an email when it is ready.")
     }
 }
